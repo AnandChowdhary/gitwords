8091ec883c5903dd9882e514af1acf8db7d31ac2
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-16 21:54:21 +0100

    :recycle: Add JSON response to delete

--- api/delete.ts
+++ api/delete.ts
@@ -24,6 +24,7 @@ export default async (req: NowRequest, res: NowResponse) => {
       path,
       sha
     });
+    return res.json({ success: true });
   } catch (error) {
     res.status(500);
     console.error(error);
