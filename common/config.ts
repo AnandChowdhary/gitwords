cca9b655f9bff6ec6a3834da2f47414e4038d78d
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-17 16:02:37 +0100

    :wrench: Update repo config

--- common/config.ts
+++ common/config.ts
@@ -1,7 +1,7 @@
 export const OWNER = "AnandChowdhary";
-export const REPO = "writing-private";
+export const REPO = "words-writing";
 export const PASSWORD_PATH = "password.txt";
-export const SECRET_PATH = ""; // optional
+export const SECRET_PATH = "secret.txt"; // optional (set as "")
 export const SALT_ROUNDS = 10;
 export const JWT_EXPIRY = "24h";
 export const UPDATE_MESSAGE = ":pencil: Update file";
